7b45dc2dfcdb7f4ca2a2ab04280988dcd705f3e2
Yan <89448433+99ru@users.noreply.github.com>
2022-04-06 09:55:50 +0200

    push before creating new branch

--- app.js
+++ app.js
@@ -2,17 +2,13 @@ const express = require("express");
 const Sequelize = require('sequelize')
 const app = express();
 
-
-// Static Files
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-// Set Views
 app.set("view engine", "ejs");
 const db = new Sequelize('sqlite://database/yummy.sqlite')            
 
 
-// Home page + gets all flavours
 app.get("/", async(req, res) => {
   const flavourList = await db.query('SELECT * FROM flavours', 
   {type: Sequelize.QueryTypes.SELECT})
@@ -20,7 +16,6 @@ app.get("/", async(req, res) => {
 });
 
 
-//  Toplist + flavours ordered by votes
 app.get("/toplist", async(req, res) => {
   const flavourList = await db.query('SELECT * FROM flavours ORDER BY votes DESC',
   {type: Sequelize.QueryTypes.SELECT})
@@ -28,19 +23,14 @@ app.get("/toplist", async(req, res) => {
 });
 
 
-// vote form
 app.post("/vote", async(req, res) => {
   const flavour = req.body.flavour; 
   const newVote = await db.query(`SELECT votes FROM flavours WHERE title = '${flavour}'`, {type: Sequelize.QueryTypes.SELECT})
-
+  
   const updateVotes = newVote[0].votes + 1
     await db.query(`UPDATE flavours SET votes = ${updateVotes} WHERE title = '${flavour}'`, {type: Sequelize.QueryTypes.UPDATE})
     res.redirect('/toplist')
 })
-
-
-
-
 
 const port = 8080;
 app.listen(port, () => {
