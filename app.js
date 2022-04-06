b6b8283bbb5715acd7f5b92e6300f01145d97755
Yan <89448433+99ru@users.noreply.github.com>
2022-04-06 12:25:05 +0200

    cleaner code

--- app.js
+++ app.js
@@ -3,19 +3,16 @@ const express = require("express");
 const app = express();
 const Flavours = require("./models/flavours");
 
-
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
-
 app.get("/", async (req, res) => {
   const flavours = await Flavours.findAll();
   res.render("index", { flavours });
 });
-
 
 app.get("/toplist", async (req, res) => {
   const flavourList = await Flavours.findAll({
@@ -25,15 +22,24 @@ app.get("/toplist", async (req, res) => {
   res.render("toplist", { flavourList });
 });
 
-
 app.post("/vote", async (req, res) => {
- // vote flavour.title
- const flavour = req.body.flavour; 
+  const flavour = req.body.flavour;
   const flavourToVote = await Flavours.findOne({ where: { title: flavour } });
-  const updateVotes = await Flavours.update( { votes: flavourToVote.votes + 1 }, { where: { title: flavour } });
-  res.redirect("/toplist");
-})
 
+  const updateVotes = await Flavours.update(
+    { votes: flavourToVote.votes + 1 },
+    { where: { title: flavour } }
+  );
+
+  res.redirect("/toplist");
+});
+
+const port = 8080;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
+// OLD VOTE
 
 /*
  app.post("/vote", async(req, res) => {
@@ -44,8 +50,3 @@ app.post("/vote", async (req, res) => {
     await db.query(`UPDATE flavours SET votes = ${updateVotes} WHERE title = '${flavour}'`, {type: Sequelize.QueryTypes.UPDATE})
     res.redirect('/toplist')
 }) */
-
-const port = 8080;
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
